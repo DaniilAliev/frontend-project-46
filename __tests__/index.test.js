a9ad5cdfbccd4f3b7d79f0635ee7b89cae51610b
DaniilAliev <de.aliev@yandex.ru>
2023-02-23 09:14:44 +0300

    fixing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import genDiff from '../src/index.js';
+import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,12 +10,14 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-const cases = [['file1.json', 'file2.json', 'resulttestjson.txt', 'stylish'], ['file1.yml', 'file2.yml', 'resulttestyml.txt', 'stylish'], ['file1.json', 'file2.json', 'resultplainjson.txt', 'plain'], ['file1.yml', 'file2.yml', 'resultplainyml.txt', 'plain']];
+const cases = [['json', 'resultstylish.txt', 'stylish'], ['yml', 'resultstylish.txt', 'stylish'], ['json', 'resultplain.txt', 'plain'], ['yml', 'resultplain.txt', 'plain']];
 
-test.each(cases)('Compare files', (firstName, secondName, expectedResult, format) => {
-  const firstFile = getFixturePath(firstName);
-  const secondFile = getFixturePath(secondName);
+test.each(cases)('Compare files', (formatFile, expectedResult, format) => {
+  const firstFile = getFixturePath(`file1.${formatFile}`);
+  const secondFile = getFixturePath(`file2.${formatFile}`);
   const getResult = readFile(expectedResult);
   const result = genDiff(firstFile, secondFile, format);
   expect(result).toEqual(getResult);
+  const data = genDiff(firstFile, secondFile, 'json');
+  expect(() => JSON.parse(data)).not.toThrow();
 });
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { readFileSync } from 'fs';
 import path from 'path';
-import parseFile from './parses.js';
-import buildTree from './buildTree.js';
-import format from './formtters/index.js';
+import parseFile from './src/parses.js';
+import buildTree from './src/buildTree.js';
+import format from './src/formtters/index.js';
 
 const readFile = (filepath) => readFileSync(path.resolve(process.cwd(), filepath.trim()), 'utf-8');
 const readFormat = (filepath) => path.extname(filepath).slice(1);
@@ -15,7 +15,6 @@ const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const parseData1 = parseFile(data1, format1);
   const parseData2 = parseFile(data2, format2);
   const innerTree = buildTree(parseData1, parseData2);
-  // return innerTree;
   return format(innerTree, formatName);
 };
 
